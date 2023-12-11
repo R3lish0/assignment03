f92b73d35bd866043d4cc5d396aeb4a608925265
Eli Newlin <elinewlin@gmail.com>
2023-12-10 22:18:52 -0600

    done

--- src/App.js
+++ src/App.js
@@ -11,72 +11,59 @@ function App() {
     const [oneProduct, setOneProduct] = useState([]);
     // new Product
     const [addNewProduct, setAddNewProduct] = useState({
-    id: 0,
+    id: null,
     title: "",
-    price: 0.0,
+    price: null,
     description: "",
     category: "",
-    image: "http://127.0.0.1:4000/images/",
-    rating: 0.0,
+    image: "",
+    rating: {rate: null,
+            count:null}
     });
 
+    const [editProductTitle, setEditProductTitle] = useState({
+        id: 0,
+        title: ""
+        });
 
+    const [reload, setReload] = useState(false);
     const [viewer1, setViewer1] = useState(false);
-    const [viewer2, setViewer2] = useState(false);
     const [viewer4, setViewer4] = useState(false);
     const [checked4, setChecked4] = useState(false);
     const [index, setIndex] = useState(0);
 
 
     const showAllItems = product.map((el) => (
+        
         <div key={el.id}>
         <img src={el.image} width={30} alt="images" /> <br />
         Title: {el.title} <br />
         Category: {el.category} <br />
         Price: {el.price} <br />
-        Rating :{el.rating} <br />
+        Rating :{el.rating.rate} <br />
         </div>
         ));
 
 
-    function getOneProduct(id) 
+    function editOneProduct() 
     {
-        console.log(id);
-        if (id >= 1 && id <= 20) 
-        {
-            fetch("http://localhost:3000/api/item/" + id)
-            .then((response) => response.json())
-            .then((data) => {
-            console.log("Show one product :", id);
-            console.log(data);
+            console.log(editProductTitle)
+            fetch("http://192.168.4.227:8080/api/item", {
+                method: "PUT",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({"id":Number(editProductTitle.id), "title":editProductTitle.title}),
+            })
             // const dataArr = [];
             // dataArr.push(data);
-            setOneProduct(data);
-        });
-        setViewer2(!viewer2);
-        }
-        else 
-        {
-            console.log("Wrong number of Product id.");
-        }
     }
 
-    const showOneItem = oneProduct.map((el) => (
-            <div key={el.id}>
-            <img src={el.image} width={30} alt="images" /> <br />
-            Title: {el.title} <br />
-            Category: {el.category} <br />
-            Price: {el.price} <br />
-            Rating: {el.rating} <br />
-            </div>
-            ));
 
     function handleChange(evt) 
     {
             const value = evt.target.value;
             if (evt.target.name === "id") 
             {
-                setAddNewProduct({ ...addNewProduct, id: value });
+                setAddNewProduct({ ...addNewProduct, id: Number(value) });
             } 
             else if (evt.target.name === "title") 
             {
@@ -92,7 +79,7 @@ function App() {
             } 
             else if (evt.target.name === "category") 
             {
-                setAddNewProduct({ ...addNewProduct, category: value });
+            setAddNewProduct({ ...addNewProduct, category: value });
             } 
             else if (evt.target.name === "image") 
             {
@@ -100,7 +87,16 @@ function App() {
 	        }
 	        else if (evt.target.name === "rating") 
 	        {
-            setAddNewProduct({ ...addNewProduct, rating: value });
+            setAddNewProduct({ ...addNewProduct, rating: { rate: value,
+                                                            count: 0}, });
+            }
+            else if (evt.target.name === "tId")
+            {
+                setEditProductTitle({ ...editProductTitle, id: value });
+            }
+            else if (evt.target.name === "tTitle")
+            {
+                setEditProductTitle({ ...editProductTitle, title: value });
             }
     }
 
@@ -108,22 +104,12 @@ function App() {
 	{
 	    e.preventDefault();
 	    console.log(e.target.value);
-	    fetch("http://localhost:3000/api/create", {
+	    fetch("http://192.168.4.227:8080/api/item", {
 	    method: "POST",
 	    headers: { "Content-Type": "application/json" },
 	    body: JSON.stringify(addNewProduct),
 	    })
-	    .then((response) => response.json())
-	    .then((data) => {
-	    console.log("Post a new product completed");
-	    console.log(data);
-	    if (data) {
-	    //const keys = Object.keys(data);
-	    const value = Object.values(data);
-	    alert(value);
-	    }
-	    });
-        }
+	    };
 
     function getOneByOneProductNext() {
 	if (product.length > 0) {
@@ -143,20 +129,10 @@ function App() {
 	}
 	function deleteOneProduct(deleteid) {
 	    console.log("Product to delete :", deleteid);
-	    fetch("http://localhost:3000/api/delete", {
+	    fetch("http://192.168.4.227:8080/api/item/", {
 	    method: "DELETE",
 	    headers: { "Content-Type": "application/json" },
-	    body: JSON.stringify({"id":deleteid}),
-	    })
-	    .then((response) => response.json())
-	    .then((data) => {
-	    console.log("Delete a product completed : ", deleteid);
-	    console.log(data);
-	    if (data) {
-	    const key = Object.keys(data);
-	    const value = Object.values(data);
-	    alert(key+value);
-	    }
+        body: JSON.stringify({"id":deleteid})
 	    });
 	    //setChecked4(!checked4);
 	    getAllProducts();
@@ -167,10 +143,10 @@ function App() {
 
     useEffect(() => {
         getAllProducts();
-        }, [checked4]);
+        }, [reload]);
 
         function getAllProducts() {
-        fetch("http://localhost:3000/api/item")
+        fetch("http://192.168.4.227:8080/api/item")
         .then((response) => response.json())
         .then((data) => {
         console.log("Show Catalog of Products :");
@@ -189,21 +165,29 @@ return (
 	<h1>Catalog of Products</h1>
 	<div>
 	<h3>Show all available Products.</h3>
-	<button onClick={() => getAllProducts()}>Show All ...</button>
+	<button onClick={() => {getAllProducts()}}>Show All ...</button>
 	{viewer1 && <div>Products {showAllItems}</div>}
 	</div>
 <div>
 
 <div>
-	<h3>Show one Product by Id:</h3>
+	<h3>Change a product title by Id:</h3>
 	<input
 	type="text"
 	id="message"
-	name="message"
+	name="tId"
 	placeholder="id"
-	onChange={(e) => getOneProduct(e.target.value)}
+	onChange={handleChange}
 	/>
-	{viewer2 && <div>Product: {showOneItem}</div>}
+    <input
+	type="text"
+	id="message"
+	name="tTitle"
+	placeholder="new title"
+	onChange={handleChange}
+	/>
+    <button type="submit" onClick={() => editOneProduct()}>Presto</button>
+	
 </div>
 
 
@@ -222,7 +206,7 @@ onChange={handleChange} />
 onChange={handleChange} />
 	<input type="text" placeholder="image?" name="image" value={addNewProduct.image}
 onChange={handleChange} />
-	<input type="number" placeholder="rate?" name="rating" value={addNewProduct.rating}
+	<input type="number" placeholder="rate?" name="rating" value={addNewProduct.rating.rate}
 onChange={handleChange} />
 	<button type="submit" onClick={handleOnSubmit}>
 submit
@@ -235,10 +219,10 @@ submit
 	<div>
 	<h3>Delete one product:</h3>
 	<input type="checkbox" id="acceptdelete" name="acceptdelete" checked={checked4}
-onChange={(e) => setChecked4(!checked4)} />
+onChange={(e) => {setChecked4(!checked4)}} />
 	<button onClick={() => getOneByOneProductPrev()}>Prev</button>
 	<button onClick={() => getOneByOneProductNext()}>Next</button>
-	<button onClick={() => deleteOneProduct(product[index].id)}>
+	<button onClick={() => {deleteOneProduct(product[index].id);setIndex(0)}}>
 Delete
 	</button>
 {checked4 && (
@@ -248,7 +232,7 @@ Id:{product[index].id} <br />
 Title: {product[index].title} <br />
 Category: {product[index].category} <br />
 Price: {product[index].price} <br />
-Rating :{product[index].rating} <br />
+Rating :{product[index].rating.rate} <br />
 	</div>
 )}
 	</div>
@@ -259,6 +243,12 @@ Rating :{product[index].rating} <br />
 
 ); // return end
 } // App end
+
+
+
+
+
+
 
 
 export default App;
